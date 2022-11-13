b5fbe75156cf2931e80eae97e8ca1e9a8b623d8a
Tatiana Bialik <bialik.tat@gmail.com>
2022-11-13 22:53:58 +0200

    fix: return username when log in

--- controllers/users.js
+++ controllers/users.js
@@ -66,7 +66,7 @@ module.exports.login = (req, res, next) => {
         NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret-key',
         { expiresIn: '7d' },
       );
-      res.send({ token });
+      res.send({ token, name: user.name });
     })
     .catch(next);
 };
